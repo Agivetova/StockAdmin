cd536372b718d9c0f74a7000c967d5bc01b78e44
agivetova <agivetovag@gmail.com>
2019-11-29 13:58:20 +0600

    Additional changes

--- src/main/java/kg/gulnaz/stock/StockForm.java
+++ src/main/java/kg/gulnaz/stock/StockForm.java
@@ -1,50 +1,56 @@
 package kg.gulnaz.stock;
 
 import com.vaadin.data.Binder;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
 
 public class StockForm extends FormLayout {
 
-    private TextField name = new TextField("Name");
-    private TextField price = new TextField("Price");
+    private TextField firstName = new TextField("Name");
+    private TextField lastName = new TextField("Price");
     private Button save = new Button("Save");
     private Button delete = new Button("Delete");
 
-    private Binder<Stocks> binder = new Binder<>(Stocks.class);
-
     private StockService service = StockService.getInstance();
-    private Stocks stocks;
+    private Stock stock;
     private StockPage stockPage;
+    private Binder<Stock> binder = new Binder<>(Stock.class);
 
     public StockForm(StockPage stockPage) {
-
         this.stockPage = stockPage;
 
         setSizeUndefined();
         HorizontalLayout buttons = new HorizontalLayout(save, delete);
-        addComponents(name, price, buttons);
+        addComponents(firstName, lastName, buttons);
+
+        save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+        save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+
+        binder.bindInstanceFields(this);
+
+        save.addClickListener(e -> this.save());
+        delete.addClickListener(e -> this.delete());
     }
 
-    public void setStock(Stocks stocks) {
-        this.stocks = stocks;
-        binder.setBean(stocks);
-        delete.setVisible(stocks.isPersisted());
+    public void setStock(Stock stock) {
+        this.stock = stock;
+        binder.setBean(stock);
+
+        delete.setVisible(stock.isPersisted());
         setVisible(true);
-        name.selectAll();
+        firstName.selectAll();
     }
 
     private void delete() {
-        service.delete(stocks);
+        service.delete(stock);
         stockPage.updateList();
         setVisible(false);
     }
 
     private void save() {
-        service.save(stocks);
+        service.save(stock);
         stockPage.updateList();
         setVisible(false);
     }
